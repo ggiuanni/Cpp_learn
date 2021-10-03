77d20c38620f61103475c560d0ec986ab192c9f3
ggiuanni <giovanni.csn@gmail.com>
2021-10-03 11:32:09 +0200

    manipulating integers

---
+++ variables/adding_integers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int total;
+    total = 12;
+    cout << total << endl;
+
+    total += 5;
+    cout << total << endl;
+
+    total++;
+    cout << total << endl;
+
+    return 0;
+}
---
+++ variables/multiple_variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tom, dick, harry;
+    tom = 10;
+    dick = 20;
+    harry = 3254;
+    cout << tom << endl;
+    cout << dick << endl;
+    cout << harry << endl;
+    return 0;
+}
