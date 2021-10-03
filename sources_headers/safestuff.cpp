b56216cdd1393f8f678d8db673e1822473059742
ggiuanni <giovanni.csn@gmail.com>
2021-10-03 14:57:50 +0200

    sources and headers

---
+++ sources_headers/safestuff.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+string SafeCracker(int SafeID)
+{
+    if (SafeID == 12)
+        return "13-26-16";
+    else
+        return "Safe Combination Unknown";
+}
