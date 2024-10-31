6fe593cd285b314e317a15863ff5e04dc416f147
ggiuanni <giovanni.csn@gmail.com>
2024-10-31 19:38:05 +0100

    creating stack and queue

---
+++ standard_library/stack_queue.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file stack_queue.cpp
+ * @author Giovanni Cassone (giovanni.csn@gmail.com)
+ * @brief Creating a Stack and a Queue
+ * @version 0.1
+ * @date 2024-10-31
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+void StackDemo()
+{
+    cout << "===Stack Demo===" << endl;
+    stack<int, vector<int>> MyStack;
+    
+    MyStack.push(5);
+    MyStack.push(10);
+    MyStack.push(15);
+    cout << MyStack.top() << endl;
+    
+    MyStack.pop();
+    cout << MyStack.top() << endl;
+    
+    MyStack.pop();
+    MyStack.push(40);
+    cout << MyStack.top() << endl;
+    
+    MyStack.pop();
+}
+
+void QueueDemo()
+{
+    cout << "===Queue Demo===" << endl;
+    queue<int> MyQueue;
+
+    MyQueue.push(5);
+    MyQueue.push(10);
+    MyQueue.push(15);
+    cout << MyQueue.front() << endl;
+
+    MyQueue.pop();
+    cout << MyQueue.front() << endl;
+
+    MyQueue.pop();
+    MyQueue.push(40);
+
+    cout << MyQueue.front() << endl;
+    MyQueue.pop();
+}
+
+int main()
+{
+    StackDemo();
+    QueueDemo();
+    
+    return 0;
+}
